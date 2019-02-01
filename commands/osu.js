57f2fa3187a70a052e5c3c8ca949cb2380a4ba46
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-01 09:44:48 +0700

    Create osu.js

---
+++ commands/osu.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+//npm i node-osu
+const osu = require('node-osu');
+// Khoa vào link này để lấy osu api nhé: https://osu.ppy.sh/p/api
+const api = new osu.Api('sau đó cho osu api vào đây',{
+	  notFoundAsError: true,
+    completeScores: false 
+})
+
+module.exports.run = async(bot,message,args) =>{
+  if(message.guild.id === '420246254254030856'){
+      if(message.channel.id === '456859356382625793' || message.channel.id === '473725146067238952' ){
+	        osu(bot,message,args);
+      } else return message.channel.send('Bạn chỉ có thể sử dụng câu lệnh này ở <#456859356382625793> và <#473725146067238952>').then(msg =>{
+		        msg.delete(3000);
+	     });
+    } else osu(bot,message,args);
+}
+
+async function osu(bot,message,args) {
+  let username = args[0];
+
+	if(!args[0]) return message.channel.send('Bạn phải nhập tên người chơi đã :3').then(msg =>{
+		msg.delete(3000);
+	})
+
+	api.getUser({u: username}).then(user =>{
+		let flag = String(user.country).toLowerCase();
+		const OsuEmbed = new Discord.RichEmbed()
+			.setTitle('Tìm kiếm thông tin người chơi')
+			.setThumbnail(`http://s.ppy.sh/a/${user.id}}`)
+			.setColor("#f442ce")
+			.addField(':large_blue_diamond: Nickname: ',user.name,true)
+			.addField(':large_blue_diamond: PP: ',Math.round(user.pp.raw),true)
+			.addField(':large_blue_diamond: Rank: ',user.pp.rank,true)
+			.addField(':large_blue_diamond: Level: ',Math.round(user.level),true) 
+			.addField(`:large_blue_diamond: Quốc gia: `,`:flag_${flag}:`,true)
+			.addField(`:large_blue_diamond: Rank :flag_${flag}: :`, `${user.pp.countryRank}`,true)
+			.addField(':large_blue_diamond: Số lần chơi: ',user.counts.plays,true)
+			.addField(':large_blue_diamond: Độ chính xác: ',user.accuracyFormatted,true)
+		message.channel.send(OsuEmbed);
+	})
+}
+module.exports.help ={
+	name: 'osu',
+  category: 'Fun',
+  description: "Dùng để lấy thông tin của người chơi trong osu",
+  usage: "do.osu [tên người chơi]"
+}
