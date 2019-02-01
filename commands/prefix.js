27817cc00cdbd1dc975241b8ea504fff7c88c766
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-01 09:48:48 +0700

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,9 @@
 module.exports.run = function() {
-    message.channel.sendMessage("My prefix is =)");
+    message.channel.sendMessage("My prefix is ``do.``");
 }
 module.exports.help = {
-    name : "prefix"
+    name : "prefix",
+    category: 'Fun',
+    description: 'Cho bạn biết prefix của Doraku Bot',
+    usage: 'do.prefix'
 }
