766b25d13c1af8a56c8664a1c8e08db7e31b0f00
Waterafall <36724510+khoatdk007@users.noreply.github.com>
2019-02-13 20:03:13 +0700

    Update hitdrama.js

--- hitdrama.js
+++ hitdrama.js
@@ -2,5 +2,5 @@ module.exports.run = function(bot, message, args) {
     if (message.content.toLowerCase().includes('drama'))
         message.channel.send(`Hít hà, hít hà \:joy:`);
     if (message.content.toLowerCase().includes('hóng')) 
-        message.channel.send(`Chúng ta cùng hóng drama nào \:grin:`);
+        message.channel.send(`Chúng ta cùng hóng nào \:grin:`);
 }
